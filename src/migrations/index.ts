2b9cd60f69f686db5b7f3349e021a9550f9f281b
Miracle Friday <miraclef60@gmail.com>
2026-09-24 10:37:57 +0100

    refactor: Fix media upload db error

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260924_082305 from './20260924_082305';
 import * as migration_20260924_100000 from './20260924_100000';
+import * as migration_20260924_100500 from './20260924_100500';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20260924_100000.up,
     down: migration_20260924_100000.down,
     name: '20260924_100000'
+  },
+  {
+    up: migration_20260924_100500.up,
+    down: migration_20260924_100500.down,
+    name: '20260924_100500'
   },
 ];
